6d867677c2a513e4449994554919931b604a0a92
zhangyuwen <zhangyuwen1987@gmail.com>
2018-02-13 03:30:37 +0800

    init project
    Signed-off-by: zhangyuwen <zhangyuwen1987@gmail.com>

---
+++ py/__init__.py
@@ -0,0 +1,13 @@
+# coding:utf-8
+#
+# @author zhangyuwen1987@gmail.com
+# @date   2018/02/13
+#
+
+
+def main():
+    pass
+
+
+if __name__ == '__main__':
+    main()
---
+++ py/hellopython.py
@@ -0,0 +1,13 @@
+# coding:utf-8
+#
+# @author zhangyuwen1987@gmail.com
+# @date   2018/02/13
+#
+
+
+def main():
+    print('Hello Python!')
+
+
+if __name__ == '__main__':
+    main()
